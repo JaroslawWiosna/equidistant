e56b3c2638b1b0740ffaf40cf731dedd920f271e
Jaroslaw Wiosna <179892@edu.p.lodz.pl>
2016-11-07 12:49:27 +0100

    Multiple points (spheres)

--- Sphere.cxx
+++ Sphere.cxx
@@ -1,150 +1,109 @@
-// SOURCE: http://www.vtk.org/Wiki/VTK/Examples/Cxx/GeometricObjects/Sphere
-// http://www.vtk.org/Wiki/VTK/Examples/Cxx/Visualization/MultipleActors
-// http://www.vtk.org/Wiki/VTK/Examples/Cxx/GeometricObjects/Point
-// http://www.vtk.org/Wiki/VTK/Examples/Cxx/PolyData/ColoredPoints
-// http://www.vtk.org/Wiki/VTK/Examples/Cxx/Images/CombineImages
-//
-#include <vtkSphereSource.h>
-#include <vtkPolyData.h>
-#include <vtkSmartPointer.h>
+// based on http://www.vtk.org/Wiki/VTK/Examples/Cxx/Visualization/MultipleActors
+
 #include <vtkPolyDataMapper.h>
 #include <vtkActor.h>
 #include <vtkRenderWindow.h>
 #include <vtkRenderer.h>
 #include <vtkRenderWindowInteractor.h>
-
-#include <vtkVersion.h>
-//#include <vtkSmartPointer.h>
-#include <vtkPoints.h>
-#include <vtkCellArray.h>
-//#include <vtkPolyData.h>
-//#include <vtkPolyDataMapper.h>
-//#include <vtkActor.h>
-#include <vtkProperty.h>
-//#include <vtkRenderWindow.h>
-//#include <vtkRenderer.h>
-//#include <vtkRenderWindowInteractor.h>
-
-//#include <vtkVersion.h>
-//#include <vtkSmartPointer.h>
-//#include <vtkPoints.h>
-//#include <vtkPolyData.h>
-#include <vtkPointData.h>
-//#include <vtkCellArray.h>
-#include <vtkUnsignedCharArray.h>
-//#include <vtkPolyDataMapper.h>
-//#include <vtkActor.h>
-//#include <vtkRenderWindow.h>
-//#include <vtkRenderer.h>
-#include <vtkRenderWindowInteractor.h>
-#include <vtkVertexGlyphFilter.h>
-//#include <vtkProperty.h>
-
-#include <vtkRenderWindow.h>
-#include <vtkRenderWindowInteractor.h>
-#include <vtkRenderer.h>
-#include <vtkJPEGReader.h>
-#include <vtkImageData.h>
+#include <vtkPolyData.h>
 #include <vtkSmartPointer.h>
-#include <vtkImageViewer2.h>
-#include <vtkImageBlend.h>
-#include <string>
-
-// For compatibility with new VTK generic data arrays
- #ifdef vtkGenericDataArray_h
- #define InsertNextTupleValue InsertNextTypedTuple
- #endif
-
-int main(int, char *[])
+#include <vtkSphereSource.h>
+#include <vtkInteractorStyleTrackballCamera.h>
+#include <vtkInteractorStyleTrackball.h>
+ 
+int main (int, char *[])
 {
-  // Create a sphere
-  vtkSmartPointer<vtkSphereSource> sphereSource = 
+  // Sphere 1
+  vtkSmartPointer<vtkSphereSource> sphereSource1 = 
     vtkSmartPointer<vtkSphereSource>::New();
-  sphereSource->SetCenter(0.0, 0.0, 0.0);
-  sphereSource->SetRadius(1.0);
-/////////////// 
-
-  vtkSmartPointer<vtkPoints> points =
-    vtkSmartPointer<vtkPoints>::New();
-  points->InsertNextPoint (0.0, 0.0, 1.0);
-  points->InsertNextPoint (1.0, 0.0, 0.0);
-  points->InsertNextPoint (0.0, 1.0, 0.0);
+  sphereSource1->SetCenter(0.0, 0.0, 0.0);
+  sphereSource1->SetRadius(1.0);
+  sphereSource1->Update();
  
-  vtkSmartPointer<vtkPolyData> pointsPolydata =
-    vtkSmartPointer<vtkPolyData>::New();
- 
-  pointsPolydata->SetPoints(points);
- 
-  vtkSmartPointer<vtkVertexGlyphFilter> vertexFilter =
-    vtkSmartPointer<vtkVertexGlyphFilter>::New();
-#if VTK_MAJOR_VERSION <= 5
-  vertexFilter->SetInputConnection(pointsPolydata->GetProducerPort());
-#else
-  vertexFilter->SetInputData(pointsPolydata);
-#endif
-  vertexFilter->Update();
- 
-  vtkSmartPointer<vtkPolyData> polydata =
-    vtkSmartPointer<vtkPolyData>::New();
-  polydata->ShallowCopy(vertexFilter->GetOutput());
- 
-  // Setup colors
-  unsigned char red[3] = {255, 0, 0};
-  unsigned char green[3] = {0, 255, 0};
-  unsigned char blue[3] = {0, 0, 255};
- 
-  vtkSmartPointer<vtkUnsignedCharArray> colors =
-    vtkSmartPointer<vtkUnsignedCharArray>::New();
-  colors->SetNumberOfComponents(3);
-  colors->SetName ("Colors");
-  colors->InsertNextTupleValue(red);
-  colors->InsertNextTupleValue(green);
-  colors->InsertNextTupleValue(blue);
- 
-  polydata->GetPointData()->SetScalars(colors);
- 
-  // Visualization
-  vtkSmartPointer<vtkPolyDataMapper> mapper =
+  vtkSmartPointer<vtkPolyDataMapper> mapper1 = 
     vtkSmartPointer<vtkPolyDataMapper>::New();
-#if VTK_MAJOR_VERSION <= 5
-  mapper->SetInputConnection(polydata->GetProducerPort());
-#else
-  mapper->SetInputData(polydata);
-#endif
+  mapper1->SetInputConnection(sphereSource1->GetOutputPort());
  
-  vtkSmartPointer<vtkActor> actorPoints =
+  vtkSmartPointer<vtkActor> actor1 = 
     vtkSmartPointer<vtkActor>::New();
-  actorPoints->SetMapper(mapper);
-  actorPoints->GetProperty()->SetPointSize(5);
-
-  vtkSmartPointer<vtkImageBlend> blend =
-    vtkSmartPointer<vtkImageBlend>::New();
-  blend->AddInputConnection(sphereSource->GetOutputPort());
-  //blend->AddInputConnection(points->GetPoint(1)->GetOutputPort());
-  blend->AddInputConnection(points->GetData());
-
-///////////////////////
-//  vtkSmartPointer<vtkPolyDataMapper> mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
-//  mapper->SetInputConnection(sphereSource->GetOutputPort());
+  actor1->SetMapper(mapper1);
  
-  vtkSmartPointer<vtkActor> actor = 
+  // Sphere 2
+  vtkSmartPointer<vtkSphereSource> sphereSource2 = 
+    vtkSmartPointer<vtkSphereSource>::New();
+  sphereSource2->SetCenter(1.0, 0.0, 0.0);
+  sphereSource2->SetRadius(0.1);
+ 
+  // Create a mapper
+  vtkSmartPointer<vtkPolyDataMapper> mapper2 = 
+    vtkSmartPointer<vtkPolyDataMapper>::New();
+  mapper2->SetInputConnection(sphereSource2->GetOutputPort());
+ 
+  // Create an actor
+  vtkSmartPointer<vtkActor> actor2 = 
     vtkSmartPointer<vtkActor>::New();
-  actor->SetMapper(mapper);
-
+  actor2->SetMapper(mapper2);
+ 
+   // Sphere 3
+  vtkSmartPointer<vtkSphereSource> sphereSource3 = 
+    vtkSmartPointer<vtkSphereSource>::New();
+  sphereSource3->SetCenter(0.0, 0.0, 1.0);
+  sphereSource3->SetRadius(0.1);
+ 
+  // Create a mapper
+  vtkSmartPointer<vtkPolyDataMapper> mapper3 = 
+    vtkSmartPointer<vtkPolyDataMapper>::New();
+  mapper3->SetInputConnection(sphereSource3->GetOutputPort());
+ 
+  // Create an actor
+  vtkSmartPointer<vtkActor> actor3 = 
+    vtkSmartPointer<vtkActor>::New();
+  actor3->SetMapper(mapper3);
+ 
+   // Sphere 4
+  vtkSmartPointer<vtkSphereSource> sphereSource4 = 
+    vtkSmartPointer<vtkSphereSource>::New();
+  sphereSource4->SetCenter(0.0, 1.0, 0.0);
+  sphereSource4->SetRadius(0.1);
+ 
+  // Create a mapper
+  vtkSmartPointer<vtkPolyDataMapper> mapper4 = 
+    vtkSmartPointer<vtkPolyDataMapper>::New();
+  mapper4->SetInputConnection(sphereSource4->GetOutputPort());
+ 
+  // Create an actor
+  vtkSmartPointer<vtkActor> actor4 = 
+    vtkSmartPointer<vtkActor>::New();
+  actor4->SetMapper(mapper4);
+ 
+  // A renderer and render window
   vtkSmartPointer<vtkRenderer> renderer = 
     vtkSmartPointer<vtkRenderer>::New();
   vtkSmartPointer<vtkRenderWindow> renderWindow = 
     vtkSmartPointer<vtkRenderWindow>::New();
   renderWindow->AddRenderer(renderer);
+ 
+  // An interactor
   vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor = 
     vtkSmartPointer<vtkRenderWindowInteractor>::New();
   renderWindowInteractor->SetRenderWindow(renderWindow);
  
-//  renderer->AddActor(actor);
-  renderer->AddActor(actorPoints);
-  renderer->SetBackground(.3, .6, .3); // Background color green
+  // Add the actors to the scene
+//  renderer->AddActor(actor1);
+  renderer->AddActor(actor2);
+  renderer->AddActor(actor3);
+  renderer->AddActor(actor4);
+  renderer->SetBackground(1,1,0); // Background color white
  
+  // Render
   renderWindow->Render();
+ 
+  vtkSmartPointer<vtkInteractorStyleTrackballCamera> style = 
+    vtkSmartPointer<vtkInteractorStyleTrackballCamera>::New(); //like paraview
+ 
+  renderWindowInteractor->SetInteractorStyle( style );
+ 
+  // Begin mouse interaction
   renderWindowInteractor->Start();
  
   return EXIT_SUCCESS;
