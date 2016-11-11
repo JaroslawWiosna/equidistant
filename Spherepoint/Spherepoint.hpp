47a3bd93ffae54251797e1038189abb663413a11
Jaroslaw Wiosna <179892@edu.p.lodz.pl>
2016-11-11 17:12:01 +0100

    getDistanceTo implemented

--- Spherepoint/Spherepoint.hpp
+++ Spherepoint/Spherepoint.hpp
@@ -9,6 +9,7 @@ class Spherepoint{
 	float phi, theta;
 	float x,y,z;
 	void setCoord();	
+	float getDistanceTo(const Spherepoint* point); 
 	Spherepoint(float PHI = 0, float THETA = 0) : phi(PHI), theta(THETA) {
 		this->setCoord();
 	} 
@@ -18,4 +19,10 @@ void Spherepoint::setCoord () {
 	this->x = cos(theta) * cos(phi);
 	this->y = cos(theta) * sin(phi);
 	this->z = sin(theta);
+}
+
+float Spherepoint::getDistanceTo(const Spherepoint* point) {
+	return sqrt( pow(this->x - point->x,2) 
+		+ pow(this->x - point->x,2) 
+		+ pow(this->z - point->z,2));
 }
