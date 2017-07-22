a20ba0d2e629c84076ea4cee999b48f322023ec5
Jaroslaw Wiosna <179892@edu.p.lodz.pl>
2017-07-22 13:55:38 +0200

    Random point picking introduced

---
+++ SphereVisualization/SphereVisualization.hpp
@@ -0,0 +1,51 @@
+/**
+ * \class SphereVisualization
+ *
+ * @par Licence
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#ifndef SPHEREVISUALIZATION_HPP
+#define SPHEREVISUALIZATION_HPP
+
+#include<iostream>
+#include<list>
+#include"Spherepoint.hpp"
+
+#include <vtkPolyDataMapper.h>
+#include <vtkActor.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkPolyData.h>
+#include <vtkSmartPointer.h>
+#include <vtkSphereSource.h>
+#include <vtkInteractorStyleTrackballCamera.h>
+#include <vtkInteractorStyleTrackball.h>
+
+#include <vtkPolyData.h>
+#include <vtkSTLWriter.h>
+#include <vtkSTLReader.h>
+#include <vtkSphereSource.h>
+#include <vtkSmartPointer.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkActor.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindowInteractor.h>
+
+class SphereVisualization{
+  public:
+    SphereVisualization(std::list<Spherepoint*>); 
+    void showAll();
+    void show();
+  private:
+    std::list<Spherepoint*> Spoints;
+};
+
+#endif // SPHEREVISUALIZATION_HPP
+
+
--- Spherepoint/Spherepoint.hpp
+++ Spherepoint/Spherepoint.hpp
@@ -1,3 +1,13 @@
+/**
+ * \class Spherepoint
+ *
+ * @par Licence
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
 #ifndef SPHEREPOINT_HPP
 #define SPHEREPOINT_HPP
 
@@ -6,61 +16,20 @@
 #include<cfloat> // FLT_MIN
 #include<cmath> // abs
 #include<algorithm>
-float PI = 3.14; // FIXME: http://stackoverflow.com/questions/1727881/how-to-use-the-pi-constant-in-c
 
 class Spherepoint{
-	public:
-	float phi, theta;
-	float x,y,z;
-	void setCoord();	
-	float getDistanceTo(Spherepoint* point); 
-	float getDistanceTo(Spherepoint& point); 
-	void rotate(float thetaMovement);
-	Spherepoint(float PHI = 0, float THETA = 0) : phi(PHI), theta(THETA) {
-		this->setCoord();
-	} 
+  public:
+    float phi, theta;
+    float x,y,z;
+    void setCoord();	
+    float getDistanceTo(Spherepoint* point); 
+    float getDistanceTo(Spherepoint& point); 
+    void rotate(float thetaMovement);
+    void setPhi(float PHI);
+    void setTheta(float THETA);
+    Spherepoint(float PHI, float THETA); 
+    Spherepoint& operator=(const Spherepoint&);
 };
-
-void Spherepoint::setCoord () {
-	this->x = cos(theta) * cos(phi);
-	this->y = cos(theta) * sin(phi);
-	this->z = sin(theta);
-}
-
-float Spherepoint::getDistanceTo(Spherepoint* point) {
-	return sqrt( pow(this->x - point->x,2) 
-		+ pow(this->y - point->y,2) 
-		+ pow(this->z - point->z,2));
-}
-
-float Spherepoint::getDistanceTo(Spherepoint& point) {
-	return sqrt( pow(this->x - point.x,2) 
-		+ pow(this->y - point.y,2) 
-		+ pow(this->z - point.z,2));
-}
-
-void Spherepoint::rotate(float thetaMovement) {
-	this->theta += thetaMovement;
-
-	if (this->theta > PI / 2.0) {
-		this->theta = PI - this->theta; // PI / 2.0 - (this->theta - PI / 2.0);
-		this->phi += PI;
-		if (this->phi > 2.0*PI) {
-			this->phi -= 2.0*PI;
-		}
-	}
-	
-	if (this->theta < (-1) * PI / 2.0) {
-		this->theta = (-1) * PI - this->theta;
-		this->phi += PI;
-		if (this->phi < 0) {
-			this->phi += 2.0*PI;
-		}
-	}
-	
-	this->setCoord();
-	
-}
 
 #endif // SPHEREPOINT_HPP
 
